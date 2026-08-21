0c834006abfade168c20eaf8bef404175183a300
Rutuja <rutujadarade2005@gmail.com>
2026-08-21 21:54:02 +0530

    test: add tests for /api/users endpoint to verify user data response

--- tests/server.test.js
+++ tests/server.test.js
@@ -22,3 +22,16 @@ describe("GET /health", () => {
     expect(response.body.status).toBe("UP");
   });
 });
+
+describe("GET /api/users", () => {
+  test("should return users", async () => {
+    const response = await request(app).get("/api/users");
+
+    expect(response.statusCode).toBe(200);
+
+    expect(response.body).toHaveLength(2);
+
+    expect(response.body[0].name).toBe("Rutuja");
+    expect(response.body[1].name).toBe("John");
+  });
+});
