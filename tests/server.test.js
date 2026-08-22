e8b392d7858f840caad5779dba1465b7f2ce9f5f
Rutuja <rutujadarade2005@gmail.com>
2026-08-22 06:24:50 +0530

    test: update application message in root endpoint test

--- tests/server.test.js
+++ tests/server.test.js
@@ -8,7 +8,7 @@ describe("GET /", () => {
     expect(response.statusCode).toBe(200);
 
     expect(response.body.message).toBe(
-      "Something else!"
+      "DevOps Platform is running!"
     );
   });
 });
